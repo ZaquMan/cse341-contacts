953db09af331b9f8306f62552f26ff6946009865
Zach Barnett <zacharybarnett96@gmail.com>
2026-01-08 20:42:22 -0800

    Node Hello World

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+
+app.use('/', routes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`application is listening on port ${port}`);
+})
